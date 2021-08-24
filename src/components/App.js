6e55e40c37aa25f4eb169edab2bdffa6bf5c8e94
unknown <74738757+leonid-br@users.noreply.github.com>
2021-08-25 02:02:26 +0300

    project preparation

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import { Component } from 'react';
+import { Style } from './App.module.css';
+// import Button from './Button';
+// import ImageGallery from './ImageGallery';
+// import ImageGalleryItem from './ImageGalleryItem';
+// import Loader from './Loader';
+// import Modal from './Modal';
+// import Searchbar from './Searchbar';
+
+class App extends Component {
+    render() {
+        return (
+            <div className={Style}>
+                {/* <Searchbar />
+                <ImageGallery />
+                <ImageGalleryItem />
+                <Loader />
+                <Button />
+                <Modal /> */}
+            </div>
+        );
+    }
+}
+
+export default App;
